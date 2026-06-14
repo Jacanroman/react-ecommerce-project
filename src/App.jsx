2f4e9e74e07fbf9d115dd86801a07d3559e8bcf5
Javier Candela <jacanroman@gmail.com>
2026-06-14 15:43:24 +0100

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import {Routes, Route} from 'react-router';
 import './App.css'
 import { HomePage } from './pages/HomePage'
 
 function App() {
 
   return (
-    <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="checkout" element={<div>Test chekout page</div>} />
+    </Routes>
   )
 }
 
